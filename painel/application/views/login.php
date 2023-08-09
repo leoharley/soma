4bceec29e4f7d6f5fbc9472a187b1af8d743d9c7
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-08-09 00:21:44 -0300

    ...

--- painel/application/views/login.php
+++ painel/application/views/login.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>ADMIN | Login do Painel Administrativo</title>
+  <title>Soma Sustentabilidade - Login do Painel Administrativo</title>
   <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
   <link href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"
