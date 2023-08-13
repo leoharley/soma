33a6997de52a60735422bbddec2218828802fe3f
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-08-12 21:57:35 -0300

    ....

--- painel/application/views/includes/header.php
+++ painel/application/views/includes/header.php
@@ -207,13 +207,13 @@ $(function() {
             ?>
             <hr>
 
-            <li class="treeview" data-toggle="collapse" data-target="#cadastro" style="margin-bottom: 10px!important;cursor: pointer!important;<?php if ($this->session->userdata('isAdmin') != 'S') { echo 'display:none'; } ?>">
+            <li class="treeview" data-toggle="collapse" data-target="#cadastro" style="margin-bottom: 10px!important;cursor: pointer!important;">
                     
                     <h4 style="margin-left:18px!important;color:white"><b><i class="fa fa-chevron-circle-down" style="margin-right:5px"></i>  CADASTRO</b></h4>
 
                     <ul class="nav nav-list collapse" style="margin-top:10px" id="cadastro">
 
-                <li class="treeview" <?php if ($this->session->userdata('isAdmin') != 'S') { echo 'style=display:none'; } ?>>
+                <li>
                   <a href="<?php echo base_url(); ?>cadastroUsuario/listar">
                     <i class="fa fa-th" style="margin-right:5px"></i>
                     <span>Usuário</span>
