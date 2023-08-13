c63dffb1c6ca4923ee2019090d6fd7536502fb23
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-08-12 22:45:22 -0300

    ...

--- painel/application/views/includes/header.php
+++ painel/application/views/includes/header.php
@@ -242,7 +242,7 @@ $(function() {
 
                   <li class="treeview">
                       <a href="<?php echo base_url(); ?>principalPlano/listar">
-                          <i class="fas fa-map" style="margin-right:5px!important"></i>
+                          <i class="fa-solid fa-map" style="margin-right:5px!important"></i>
                           <span>Propriedades</span>
                       </a>
                   </li>
