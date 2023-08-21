75ba9b1840d1da3a67659abb0482e8b550308a99
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-08-21 18:45:27 -0300

    ....

--- painel/application/views/principal/c_principalParcela.php
+++ painel/application/views/principal/c_principalParcela.php
@@ -91,11 +91,12 @@ if(!empty($infoParcela))
 
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="ds_nome">Ano emissão</label>                                        
-                                        <input type="text" class="yearpicker form-control" value="<?php echo ($this->uri->segment(2) == 'cadastrar') ? set_value('nu_ano_emissao') : $nu_ano_emissao ; ?>"  />
+                                        <label for="ds_nome">Ano emissão</label>
+                                        <input type="text" class="form-control required" value="<?php echo ($this->uri->segment(2) == 'cadastrar') ? set_value('nu_ano_emissao') : $nu_ano_emissao ; ?>" id="nu_ano_emissao" name="nu_ano_emissao">
                                         <input type="hidden" value="<?php echo $id; ?>" name="id" id="id" />
                                     </div>
-                                </div>                                        
+                                </div>
+                                        
                             </div>
 
                             <div class="row">
@@ -202,10 +203,5 @@ if(!empty($infoParcela))
 <script>
 $(document).ready(function(){
     $(":input").inputmask();
-    $(".yearpicker").yearpicker({
-          year: 2017,
-          startYear: 2012,
-          endYear: 2030
-        });
 });
 </script>
