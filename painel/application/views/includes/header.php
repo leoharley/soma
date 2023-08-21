9bbc19f76c2723a811a4c4eda0cf748da1bfb47d
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-08-21 18:35:04 -0300

    ....

--- painel/application/views/includes/header.php
+++ painel/application/views/includes/header.php
@@ -56,6 +56,7 @@
 
 <script src="<?php echo base_url(); ?>assets/plugins/input-mask/jquery.inputmask.js"></script>
 <script src="<?php echo base_url(); ?>assets/plugins/yearpicker/yearpicker.js"></script>
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/yearpicker/yearpicker.css">
 
 
 <script>
