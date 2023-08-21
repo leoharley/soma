2ed09ef5e9c9eb34e109533645b92c9d2eceb44e
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-08-21 19:44:02 -0300

    ....

--- painel/application/views/dashboard.php
+++ painel/application/views/dashboard.php
@@ -20,7 +20,7 @@
           <div class="icon">
             <i class="fa fa-tasks"></i>
           </div>
-          <a href="<?php echo base_url(); ?><?php  if($role != ROLE_EMPLOYEE) {echo 'tasks';}else{echo 'etasks';} ?>" class="small-box-footer">Mais Informações
+          <a href="<?php echo base_url(); ?>cadastroUsuario/listar" class="small-box-footer">Mais Informações
             <i class="fa fa-arrow-circle-right"></i>
           </a>
         </div>
@@ -38,7 +38,7 @@
           <div class="icon">
             <i class="ion ion-pie-graph"></i>
           </div>
-          <a href="<?php echo base_url(); ?><?php  if($role != ROLE_EMPLOYEE) {echo 'tasks';}else{echo 'etasks';} ?>" class="small-box-footer">Mais Informações
+          <a href="<?php echo base_url(); ?>principalProjeto/listar" class="small-box-footer">Mais Informações
             <i class="fa fa-arrow-circle-right"></i>
           </a>
         </div>
