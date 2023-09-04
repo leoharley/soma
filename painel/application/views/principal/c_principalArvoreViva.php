f972c8902acfcc2b0add9a127b10e8bd5974a0e8
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-09-04 10:00:58 -0300

    ....

--- painel/application/views/principal/c_principalArvoreViva.php
+++ painel/application/views/principal/c_principalArvoreViva.php
@@ -124,8 +124,7 @@ if(!empty($infoArvoreViva))
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="id_familia">Família</label>
-                                        <select class="form-control required" id="id_familia" name="id_familia" required>
-                                            <option value="" disabled selected>SELECIONE</option>
+                                        <select id="id_familia" name="id_familia" required>   
                                             <?php
                                             if(!empty($infoFamilias))
                                             {
@@ -149,8 +148,7 @@ if(!empty($infoArvoreViva))
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="id_genero">Gênero</label>
-                                        <select class="form-control required" id="id_genero" name="id_genero" required>
-                                            <option value="" disabled selected>SELECIONE</option>
+                                        <select id="id_genero" name="id_genero" required>
                                             <?php
                                             if(!empty($infoGeneros))
                                             {
@@ -171,7 +169,7 @@ if(!empty($infoArvoreViva))
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="id_especie">Espécie</label>
-                                        <select class="form-control required" id="id_especie" name="id_especie">
+                                        <select id="id_especie" name="id_especie">
                                         </select>
                                     </div>
                                 </div>
