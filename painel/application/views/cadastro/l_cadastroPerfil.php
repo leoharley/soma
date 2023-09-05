3683b0aba619c46a985e43dd35f18c6082f51d79
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-09-05 19:23:50 -0300

    ....

--- painel/application/views/cadastro/l_cadastroPerfil.php
+++ painel/application/views/cadastro/l_cadastroPerfil.php
@@ -92,6 +92,7 @@
                           <a class="btn btn-sm btn-danger deleteUser" href="<?php echo base_url().'apagaPerfil/'.$registro->id_perfil; ?>" data-userid="<?php echo $registro->id_perfil; ?>" title="Excluir">
                               <i class="fa fa-trash-o"></i>
                           </a>
+                          <a href="#" data-href="/delete.php?id=23" data-toggle="modal" data-target="#confirm-delete">Delete record #23</a>                          
                       </td>
                     </tr>
                     <?php
@@ -100,6 +101,30 @@
                       ?>
                 </tbody>
               </table>
+
+              <div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                      <div class="modal-content">
+                      
+                          <div class="modal-header">
+                              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                              <h4 class="modal-title" id="myModalLabel">Confirm Delete</h4>
+                          </div>
+                      
+                          <div class="modal-body">
+                              <p>You are about to delete one track, this procedure is irreversible.</p>
+                              <p>Do you want to proceed?</p>
+                              <p class="debug-url"></p>
+                          </div>
+                          
+                          <div class="modal-footer">
+                              <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                              <a class="btn btn-danger btn-ok">Delete</a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+
             </div>
         </div>
         <!-- /.box-body -->
