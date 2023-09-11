6c6586e36adeb89f45d6b1aed19faff4a2ba5455
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-09-11 14:34:59 -0300

    ....

--- painel/application/models/CadastroModel.php
+++ painel/application/models/CadastroModel.php
@@ -131,6 +131,8 @@ class CadastroModel extends CI_Model
                     OR ds_email = '".$Email."'))";
         $this->db->where($campos);
         $query = $this->db->get();
+
+        var_dump($this->db->last_query());exit;
         
         return $query->result();
     }
