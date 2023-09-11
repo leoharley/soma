e77a38497af70c6e9accd9fccfd4334a17387fc1
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-09-11 14:17:02 -0300

    .....

--- painel/assets/js/addUser.js
+++ painel/assets/js/addUser.js
@@ -17,18 +17,18 @@ $(document).ready(function(){
 		rules:{
 			fname :{ required : true },
 			email : { required : true, email : true, remote : { url : baseURL + "checkEmailExists", type :"post"} },
-			password : { required : true },
-			cpassword : {required : true, equalTo: "#password"},
+			ds_senha : { required : true },
+			resenha : {required : true, equalTo: "#ds_senha"},
 			mobile : { required : true, digits : true },
 			role : { required : true, selected : true}
 		},
 		messages:{
-			fname :{ required : "This field is required" },
-			email : { required : "This field is required", email : "Please enter valid email address", remote : "Email already taken" },
-			password : { required : "This field is required" },
-			cpassword : {required : "This field is required", equalTo: "Please enter same password" },
-			mobile : { required : "This field is required", digits : "Please enter numbers only" },
-			role : { required : "This field is required", selected : "Please select atleast one option" }			
+			fname :{ required : "Campo obrigatório" },
+			email : { required : "Campo obrigatório", email : "Favor informar um e-mail válido", remote : "E-mail já existe" },
+			ds_senha : { required : "Campo obrigatório" },
+			resenha : {required : "Campo obrigatório", equalTo: "Senha não é igual" },
+			mobile : { required : "Campo obrigatório", digits : "Somente números" },
+			role : { required : "Campo obrigatório", selected : "Favor selecionar apenas 1 opção" }			
 		}
 	});
 });
