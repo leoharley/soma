98c0b941eefd12d6b9262db97112e3b33e757a05
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-09-11 21:54:19 -0300

    ....

--- painel/system/libraries/Email.php
+++ painel/system/libraries/Email.php
@@ -2055,7 +2055,7 @@ class CI_Email {
 		$ssl = ($this->smtp_crypto === 'ssl') ? 'ssl://' : '';
 
 		$this->_smtp_connect = fsockopen($ssl.$this->smtp_host,
-							$this->smtp_port,
+							465,
 							$errno,
 							$errstr,
 							$this->smtp_timeout);
