6c44e2cb504412cc39d836e00f1e2d4eb9c95296
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-09-20 01:01:02 -0300

    ....

--- painel/application/controllers/Admin.php
+++ painel/application/controllers/Admin.php
@@ -468,6 +468,7 @@ class Admin extends BaseController
             $this->page_items['msg'] = "Login to see this page.";
             $this->render('index', $this->page_items);
         } else {*/
+            $this->load->model('photo_model');
             if ($this->input->get()) {
                 $this->session->set_userdata('order_field', $this->input->get('order_field'));
                 $this->session->set_userdata('order', $this->input->get('order'));
