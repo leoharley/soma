ae8c930074b4077930c6e660cdb1d45ab78767e6
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-09-20 15:29:41 -0300

    ....

--- painel/application/views/admin/photo/parts_table_photo.php
+++ painel/application/views/admin/photo/parts_table_photo.php
@@ -30,7 +30,7 @@
      ?>
     </td>
     <td><?= $photo['ds_nome']?></td>
-    <td><a href="<?= base_url($photo['link'])?>">Baixar</a></td>
+    <td><a href="<?= base_url($photo['link'])?>" target="_blank">Baixar</a></td>
     <td><?= anchor('admin/edit/'.$ds_categoria.'/'.$id_categoria.'/'. $photo['id'], 'Editar')?></td>
     <td style="vertical-align:top!important">
     <a class=""
