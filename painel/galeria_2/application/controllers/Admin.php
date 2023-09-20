91ff112137b11b3c0f73ff751d2bb41954379d84
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-09-20 07:43:56 -0300

    ....

--- painel/galeria_2/application/controllers/Admin.php
+++ painel/galeria_2/application/controllers/Admin.php
@@ -23,7 +23,6 @@ class Admin extends CI_Controller
 
         if ( ! $this->session->userdata('logged_in'))
         {
-            var_dump('teste');exit;
             // Allow some methods?
             $allowed = array(
                 'index',
@@ -43,6 +42,7 @@ class Admin extends CI_Controller
             $this->render('index', $this->page_items);
         } else {*/
 
+            var_dump('arqui');exit;
             if ($this->input->get()) {
                 $this->session->set_userdata('order_field', $this->input->get('order_field'));
                 $this->session->set_userdata('order', $this->input->get('order'));
