415e7034a272c60ccedf15b85e783f04bad0c3b1
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-09-20 16:03:28 -0300

    ....

--- painel/application/views/principal/c_principalParcela.php
+++ painel/application/views/principal/c_principalParcela.php
@@ -320,7 +320,7 @@ if(!empty($infoParcela))
 function popup(url, windowName, win, w, h) {
     const y = win.top.outerHeight / 2 + win.top.screenY - ( h / 2);
     const x = win.top.outerWidth / 2 + win.top.screenX - ( w / 2);
-    return win.open(url, windowName, `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=${w}, height=${h}, top=${y}, left=${x}`);
+    return win.open(url, windowName, 'toolbar=no, location=no, directories=no, toolbar=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=${w}, height=${h}, top=${y}, left=${x}');
 }
 
 
