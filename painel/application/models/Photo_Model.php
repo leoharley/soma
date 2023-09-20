763f9c3e077fcf7eb6b5478a9d2f696ab67b0767
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-09-20 00:49:50 -0300

    ....

--- painel/application/models/Photo_Model.php
+++ painel/application/models/Photo_Model.php
@@ -3,19 +3,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Photo_Model extends CI_Model
 {
-  public $id;
-  public $name;
-  public $desc;
-  public $date;
-  public $location;
-  public $link_thumb;
-  public $link_small;
 
-  public function __construct()
-  {
-    parent::__construct();
-    $this->load->database();
-  }
-
-  
 }
