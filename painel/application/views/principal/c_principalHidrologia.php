09864e1968e18049a94ffca8f1d909e8d198f890
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-09-26 16:57:29 -0300

    ....

--- painel/application/views/principal/c_principalHidrologia.php
+++ painel/application/views/principal/c_principalHidrologia.php
@@ -82,7 +82,7 @@ if(!empty($infoHidrologia))
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="descricao">Descrição</label>
-                                        <input type="text" class="form-control required" id="descricao" value="<?php echo ($this->uri->segment(2) == 'cadastrar') ? set_value('descricao') : $descricao; ?>" name="descricao">
+                                        <input type="textarea" class="form-control required" id="descricao" value="<?php echo ($this->uri->segment(2) == 'cadastrar') ? set_value('descricao') : $descricao; ?>" name="descricao">
                                         <input type="hidden" value="<?php echo $id; ?>" name="id" id="id" />
                                     </div>
                                 </div>
