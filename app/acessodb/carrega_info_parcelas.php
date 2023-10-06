f47d2191f9300dc6c77710b98fbcc0e7cc72bea4
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-10-05 23:08:10 -0300

    ....

---
+++ app/acessodb/carrega_info_parcelas.php
@@ -0,0 +1,26 @@
+<?php
+$response = array();
+include 'db/db_connect.php';
+include 'functions.php';
+ 
+//Get the input request parameters
+$inputJSON = file_get_contents('php://input');
+$input = json_decode($inputJSON, TRUE); //convert JSON into array
+ 
+//Check for Mandatory parameters
+
+$query    = "SELECT Parcelas.id, Propriedades.no_propriedade 
+			 FROM tb_parcelas as Parcelas 
+			 INNER JOIN tb_propriedades as Propriedades on Propriedades.id = Parcelas.id_propriedade 
+			 and Propriedades.st_registro_ativo = 'S'
+			 WHERE Parcelas.st_registro_ativo = 'S'";
+ 
+if($stmt = $con->prepare($query)){
+	$stmt->execute();
+	$response = $stmt->fetch();
+	$stmt->close();
+}
+
+//Display the JSON response
+echo json_encode($response);
+?>
