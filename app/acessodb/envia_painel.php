5fd53efb3703a5a7ed6eedf772f5569afc242051
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-10-25 23:01:04 -0300

    ....

--- app/acessodb/envia_painel.php
+++ app/acessodb/envia_painel.php
@@ -9,12 +9,13 @@ $input = json_decode($inputJSON, TRUE); //convert JSON into array
  
 //Check for Mandatory parameters
 if(isset($input['registroanimais'])){
-	$registroanimais = $input['registroanimais'];
+	//$registroanimais = $input['registroanimais'];
+	$registroanimais = 'teste';
 		
 	//Query to register new user
 	$insertQuery  = "INSERT INTO teste_envia_painel(descricao) VALUES (?)";
 	if($stmt = $con->prepare($insertQuery)){
-		$stmt->bind_param("ssss",$registroanimais[0]->id);
+		$stmt->bind_param("ssss",$registroanimais);
 		$stmt->execute();
 		$response["status"] = 0;
 		$response["message"] = "Registro animais adicionado";
