ee0e2a234f5e155c40d2a8977b2f5809f863f533
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-11-01 19:31:16 -0300

    ....

--- app/acessodb/db/db_connect.php
+++ app/acessodb/db/db_connect.php
@@ -2,7 +2,7 @@
 define('DB_USER', "u699148595_u699148595_som"); // db user
 define('DB_PASSWORD', "%Soma2023"); // db password (mention your db password here)
 define('DB_DATABASE', "u699148595_inventario_hom"); // database name
-define('DB_SERVER', "185.211.7.205:3306"); // db server
+define('DB_SERVER', "localhost:3306"); // db server
  
 
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE);
