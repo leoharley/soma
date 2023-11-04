09e57b7a6c77d69c1712176eaf2c79f87bafec41
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-11-04 10:46:10 -0300

    ....

--- painel/application/views/principal/c_principalParcela.php
+++ painel/application/views/principal/c_principalParcela.php
@@ -334,7 +334,7 @@ $(document).ready(function(){
     $(":input").inputmask();
     
     $("#longitude").inputmask({
-    mask: ['99°M9\'P9.99"', '[1]79°M9\'S9.99"'],
+    mask: ['99°M9\'P9.99"D', '[1]79°M9\'S9.99"D'],
     definitions: {
       D: {
         validator: '[nN|sS]',
@@ -357,7 +357,7 @@ $(document).ready(function(){
     });
 
         $("#latitude").inputmask({
-        mask: '89°M9\'P9.99"',
+        mask: '89°M9\'P9.99"D',
         definitions: {
         D: {
             validator: '[eE|oO]',
