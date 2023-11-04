e7fa622ac9f5f06cb1a958dd1fe71a6ec7ce775c
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-11-04 00:48:48 -0300

    ....

--- app/acessodb/envia_painel.php
+++ app/acessodb/envia_painel.php
@@ -10,8 +10,11 @@ $input = json_decode($inputJSON, TRUE); //convert JSON into array
 $latitudecampogd  = $input['latitudecampogd'];
 $longitudecampogd = $input['longitudecampogd'];
 $result = explode(" ",DDtoDMS_string($latitudecampogd,$longitudecampogd));
+	
 
-
+if ($input['dscategoria'] == 'animais') {
+	//	$input['latitudecampogd'] e $input['longitudecampogd']//AQUI SERGIONE, PEGA OS CAMPOS QUE VEM DO APP, TRANSFORMA AQUI PRA GMS E JOGA NOS CAMPOS DE GMS NO PAINEL
+	
 	$insertQuery  = "DELETE FROM tb_animais WHERE id_acesso=".$input['idacesso'];
 	if($stmt = $con->prepare($insertQuery)){
 		$stmt->execute();
@@ -19,34 +22,6 @@ $result = explode(" ",DDtoDMS_string($latitudecampogd,$longitudecampogd));
 		$response["message"] = "Enviado com sucesso!";
 		$stmt->close();
 	}
-	
-	$insertQuery  = "DELETE FROM tb_arvores_vivas WHERE id_acesso=".$input['idacesso'];
-	if($stmt = $con->prepare($insertQuery)){
-		$stmt->execute();
-		$response["status"] = 0;
-		$response["message"] = "Enviado com sucesso!";
-		$stmt->close();
-	}
-	
-	$insertQuery  = "DELETE FROM tb_epifitas WHERE id_acesso=".$input['idacesso'];
-	if($stmt = $con->prepare($insertQuery)){
-		$stmt->execute();
-		$response["status"] = 0;
-		$response["message"] = "Enviado com sucesso!";
-		$stmt->close();
-	}
-	
-	$insertQuery  = "DELETE FROM tb_hidrologia WHERE id_acesso=".$input['idacesso'];
-	if($stmt = $con->prepare($insertQuery)){
-		$stmt->execute();
-		$response["status"] = 0;
-		$response["message"] = "Enviado com sucesso!";
-		$stmt->close();
-	}
-	
-
-if ($input['dscategoria'] == 'animais') {
-	//	$input['latitudecampogd'] e $input['longitudecampogd']//AQUI SERGIONE, PEGA OS CAMPOS QUE VEM DO APP, TRANSFORMA AQUI PRA GMS E JOGA NOS CAMPOS DE GMS NO PAINEL
 	
 	$insertQuery  = "REPLACE INTO tb_animais(id,id_parcela,id_acesso,id_tipo_observacao,id_classificacao,id_grau_protecao,latitude_campo_gd,longitude_campo_gd,latitude_campo_gms,longitude_campo_gms) VALUES (?,?,?,?,?,?,?,?,?,?)";
 	if($stmt = $con->prepare($insertQuery)){
@@ -69,6 +44,15 @@ if ($input['dscategoria'] == 'animais') {
 	}
 	
 } else if ($input['dscategoria'] == 'arvoresvivas') {
+	
+	$insertQuery  = "DELETE FROM tb_arvores_vivas WHERE id_acesso=".$input['idacesso'];
+	if($stmt = $con->prepare($insertQuery)){
+		$stmt->execute();
+		$response["status"] = 0;
+		$response["message"] = "Enviado com sucesso!";
+		$stmt->close();
+	}
+	
     $insertQuery  = "REPLACE INTO tb_arvores_vivas(id,id_parcela,id_acesso,id_grau_protecao,latitude_campo_gd,longitude_campo_gd,nu_biomassa,identificacao,nu_circunferencia,nu_altura,nu_altura_total,nu_altura_fuste,nu_altura_copa,isolada,floracao_frutificacao,latitude_campo_gms,longitude_campo_gms) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 	if($stmt = $con->prepare($insertQuery)){
 		$stmt->bind_param("sssssssssssssssss",$input['idcontrolearvoresvivas'],strtok($input['idparcelaarvoresvivas'], '-'),$input['idacesso'],strtok($input['idgrauprotecao'], '-'),$input['latitudecampogd'],$input['longitudecampogd'],$input['nubiomassa'],$input['identificacao'],$input['nucircunferencia'],$input['nualtura'],$input['nualturatotal'],$input['nualturafuste'],$input['nualturacopa'],$input['isolada'],$input['floracaofrutificacao'],$result[0],$result[1]);
@@ -90,6 +74,14 @@ if ($input['dscategoria'] == 'animais') {
 	}
 	
 } else if ($input['dscategoria'] == 'epifitas') {
+	$insertQuery  = "DELETE FROM tb_epifitas WHERE id_acesso=".$input['idacesso'];
+	if($stmt = $con->prepare($insertQuery)){
+		$stmt->execute();
+		$response["status"] = 0;
+		$response["message"] = "Enviado com sucesso!";
+		$stmt->close();
+	}
+	
     $insertQuery  = "REPLACE INTO tb_epifitas(id,id_acesso,id_parcela,latitude_campo_gd,longitude_campo_gd,latitude_campo_gms,longitude_campo_gms) VALUES (?,?,?,?,?,?,?)";
 	if($stmt = $con->prepare($insertQuery)){
 		$stmt->bind_param("sssssss",$input['idcontroleepifitas'],$input['idacesso'],strtok($input['idparcelaepifitas'], '-'),$input['latitudecampogd'],$input['longitudecampogd'],$result[0],$result[1]);
@@ -111,6 +103,14 @@ if ($input['dscategoria'] == 'animais') {
 	}
 
 } else if ($input['dscategoria'] == 'hidrologia') {
+	$insertQuery  = "DELETE FROM tb_hidrologia WHERE id_acesso=".$input['idacesso'];
+	if($stmt = $con->prepare($insertQuery)){
+		$stmt->execute();
+		$response["status"] = 0;
+		$response["message"] = "Enviado com sucesso!";
+		$stmt->close();
+	}
+	
     $insertQuery  = "REPLACE INTO tb_hidrologia(id,id_parcela,id_acesso,descricao,latitude_campo_gd,longitude_campo_gd,latitude_campo_gms,longitude_campo_gms) VALUES (?,?,?,?,?,?,?,?)";
 	if($stmt = $con->prepare($insertQuery)){
 		$stmt->bind_param("ssssssss",$input['idcontrolehidrologia'],strtok($input['idparcelahidrologia'], '-'),$input['idacesso'],$input['descricao'],$input['latitudecampogd'],$input['longitudecampogd'],$result[0],$result[1]);
