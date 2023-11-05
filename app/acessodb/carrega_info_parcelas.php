1b6552e616dca13689d3d5394106af223ba677a6
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-11-05 20:41:57 -0300

    ....

--- app/acessodb/carrega_info_parcelas.php
+++ app/acessodb/carrega_info_parcelas.php
@@ -9,10 +9,9 @@ $input = json_decode($inputJSON, TRUE); //convert JSON into array
  
 //Check for Mandatory parameters
 
-$query    = "INSERT INTO teste_envia_painel(id,id_parcela,id_acesso) VALUES (?,?,?)";
+$query    = "INSERT INTO teste_envia_painel(id,id_parcela,id_acesso) VALUES (3213,'18','".$input['idacesso']."')";
  
 if($stmt = $con->prepare($query)){
-	$stmt->bind_param("sss",4554,"18",$input['idacesso']);
 	$stmt->execute();
 	$stmt->close();
 }
