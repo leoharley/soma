3adac673f06d04acb31d05393ace9e0cf7eedfb0
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-11-05 14:02:40 -0300

    ....

--- app/acessodb/carrega_info_parcelas.php
+++ app/acessodb/carrega_info_parcelas.php
@@ -14,7 +14,7 @@ $query    = "SELECT Parcelas.id, Propriedades.no_propriedade, Parcelas.latitude_
 			 INNER JOIN tb_propriedades as Propriedades on Propriedades.id = Parcelas.id_propriedade 
 			 and Propriedades.st_registro_ativo = 'S'
 			 WHERE Parcelas.st_registro_ativo = 'S'
-			 Parcelas.co_acesso = (?)";
+			 and Parcelas.co_acesso = (?)";
  
 if($stmt = $con->prepare($query)){
 	$stmt->bind_param("s",$input['idacesso']);
