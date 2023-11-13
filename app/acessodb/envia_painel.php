103d723df1dcc4fdaa89ba20dcc3fc8c17c0890e
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-11-13 09:07:55 -0300

    .....

--- app/acessodb/envia_painel.php
+++ app/acessodb/envia_painel.php
@@ -6,6 +6,7 @@ include 'functions.php';
 //Get the input request parameters
 $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, TRUE); //convert JSON into array
+$modo_debug = true;
 
 $latitudecampogd  = $input['latitudecampogd'];
 $longitudecampogd = $input['longitudecampogd'];
@@ -24,6 +25,20 @@ function getBetween($string, $start = "", $end = ""){
         return '';
     }
 }
+
+if ($modo_debug) {
+	
+$insertQuery  = "INSERT INTO teste_envia_painel(ds_campos) VALUES (?,?)";
+	if($stmt = $con->prepare($insertQuery)){
+		$stmt->bind_param("s",$input);
+		$stmt->execute();
+		$response["status"] = 0;
+		
+		$response["message"] = "Enviado com sucesso!";
+		$stmt->close();
+	}
+	
+} else {
 
 if ($input['dscategoria'] == 'limpatabelas') {
 	
@@ -63,10 +78,13 @@ if ($input['dscategoria'] == 'limpatabelas') {
 	
 } else if ($input['dscategoria'] == 'animais') {
 	//	$input['latitudecampogd'] e $input['longitudecampogd']//AQUI SERGIONE, PEGA OS CAMPOS QUE VEM DO APP, TRANSFORMA AQUI PRA GMS E JOGA NOS CAMPOS DE GMS NO PAINEL
-		
+	if (strtok($input['idtpobservacao'], '-') == 'SELECIONE'){$idtpobservacao = null;} else {$idtpobservacao = strtok($input['idtpobservacao'], '-');}
+	if (strtok($input['idclassificacao'], '-') == 'SELECIONE'){$idclassificacao = null;} else {$idclassificacao = strtok($input['idclassificacao'], '-');}
+	if (strtok($input['idgrauprotecao'], '-') == 'SELECIONE'){$idgrauprotecao = null;} else {$idgrauprotecao = strtok($input['idgrauprotecao'], '-');}
+	
 	$insertQuery  = "REPLACE INTO tb_animais(id,id_parcela,id_acesso,id_tipo_observacao,id_classificacao,id_grau_protecao,latitude_campo_gd,longitude_campo_gd,latitude_campo_gms,longitude_campo_gms,descricao) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
 	if($stmt = $con->prepare($insertQuery)){
-		$stmt->bind_param("sssssssssss",$input['idcontroleanimais'],getBetween($input['idparcelaanimais'],"(",")"),$input['idacesso'],strtok($input['idtpobservacao'], '-'),strtok($input['idclassificacao'], '-'),strtok($input['idgrauprotecao'], '-'),$input['latitudecampogd'] ,$input['longitudecampogd'],$result[0],$result[1],$input['descricao']);
+		$stmt->bind_param("sssssssssss",$input['idcontroleanimais'],getBetween($input['idparcelaanimais'],"(",")"),$input['idacesso'],$idtpobservacao,$idclassificacao,$idgrauprotecao,$input['latitudecampogd'] ,$input['longitudecampogd'],$result[0],$result[1],$input['descricao']);
 		$stmt->execute();
 		$response["status"] = 0;
 		
@@ -85,10 +103,13 @@ if ($input['dscategoria'] == 'limpatabelas') {
 	}
 	
 } else if ($input['dscategoria'] == 'arvoresvivas') {
+    if (strtok($input['idestagioregeneracao'], '-') == 'SELECIONE'){$idestagioregeneracao = null;} else {$idestagioregeneracao = strtok($input['idestagioregeneracao'], '-');}
+	if (strtok($input['idgrauepifitismo'], '-') == 'SELECIONE'){$idgrauepifitismo = null;} else {$idgrauepifitismo = strtok($input['idgrauepifitismo'], '-');}
+	if (strtok($input['idgrauprotecao'], '-') == 'SELECIONE'){$idgrauprotecao = null;} else {$idgrauprotecao = strtok($input['idgrauprotecao'], '-');}
 	
     $insertQuery  = "REPLACE INTO tb_arvores_vivas(id,id_parcela,id_acesso,id_grau_protecao,latitude_campo_gd,longitude_campo_gd,nu_biomassa,identificacao,nu_circunferencia,nu_altura,nu_altura_total,nu_altura_fuste,nu_altura_copa,isolada,floracao_frutificacao,latitude_campo_gms,longitude_campo_gms,descricao,id_estagio_regeneracao,id_grau_epifitismo) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 	if($stmt = $con->prepare($insertQuery)){
-		$stmt->bind_param("ssssssssssssssssssss",$input['idcontrolearvoresvivas'],getBetween($input['idparcelaarvoresvivas'],"(",")"),$input['idacesso'],strtok($input['idgrauprotecao'], '-'),$input['latitudecampogd'],$input['longitudecampogd'],$input['nubiomassa'],$input['identificacao'],$input['nucircunferencia'],$input['nualtura'],$input['nualturatotal'],$input['nualturafuste'],$input['nualturacopa'],$input['isolada'],$input['floracaofrutificacao'],$result[0],$result[1],$input['descricao'],strtok($input['idestagioregeneracao'], '-'),strtok($input['idgrauepifitismo'], '-'));
+		$stmt->bind_param("ssssssssssssssssssss",$input['idcontrolearvoresvivas'],getBetween($input['idparcelaarvoresvivas'],"(",")"),$input['idacesso'],$idgrauprotecao,$input['latitudecampogd'],$input['longitudecampogd'],$input['nubiomassa'],$input['identificacao'],$input['nucircunferencia'],$input['nualtura'],$input['nualturatotal'],$input['nualturafuste'],$input['nualturacopa'],$input['isolada'],$input['floracaofrutificacao'],$result[0],$result[1],$input['descricao'],$idestagioregeneracao,$idgrauepifitismo);
 		$stmt->execute();
 		$response["status"] = 0;
 		
@@ -158,7 +179,7 @@ else{
 }
 
 
-
+}
 
 
 echo json_encode($response);
