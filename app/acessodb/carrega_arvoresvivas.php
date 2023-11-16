dc1796369803e8b0c6fdb8bc3dfdcf4b24226931
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-11-15 21:57:26 -0300

    ....

---
+++ app/acessodb/carrega_arvoresvivas.php
@@ -0,0 +1,24 @@
+<?php
+$response = array();
+include 'db/db_connect.php';
+include 'functions.php';
+ 
+//Get the input request parameters
+$inputJSON = file_get_contents('php://input');
+$input = json_decode($inputJSON, TRUE); //convert JSON into array
+ 
+//Check for Mandatory parameters
+			 
+$query    = "SELECT *
+			 FROM tb_arvores_vivas as ArvoresVivas			 
+			 WHERE ArvoresVivas.st_registro_ativo = 'S'";
+ 
+if($stmt = $con->prepare($query)){
+	$stmt->execute();
+	$response = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+	$stmt->close();
+}
+
+//Display the JSON response
+echo json_encode($response);
+?>
