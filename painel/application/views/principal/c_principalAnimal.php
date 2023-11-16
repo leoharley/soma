8605310cc58b45dae4388d9554fb0a1a04d0f6f2
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-11-15 22:36:13 -0300

    ....

--- painel/application/views/principal/c_principalAnimal.php
+++ painel/application/views/principal/c_principalAnimal.php
@@ -390,8 +390,8 @@ $(document).ready(function(){
             }
         });
 
-    $('select[name="id_familia"]').on('change', function() {
-        $('select[name="id_familia"]').on('click', function() {
+  /*  $('select[name="id_familia"]').on('change', function() {
+        $('select[name="id_familia"]').on('click', function() {*/
             var idFamilia = $(this).val();
             if(idFamilia) {
                 $.ajax({
@@ -410,8 +410,8 @@ $(document).ready(function(){
         //      $('select[name="id_genero"]').empty();
         //     $('select[name="id_especie"]').empty();
             }
-        });
-    });
+   /*     });
+    });*/
 
 
     var idGenero = $('#id_genero').val();
