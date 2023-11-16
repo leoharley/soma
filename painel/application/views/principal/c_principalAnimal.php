b6df5364afcf07b981106dad336e5aa2d20a1f72
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-11-15 23:07:08 -0300

    ....

--- painel/application/views/principal/c_principalAnimal.php
+++ painel/application/views/principal/c_principalAnimal.php
@@ -73,7 +73,7 @@ if(!empty($infoAnimal))
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="id_parcela">Parcela<?=$id_parcela?></label>
-                                        <select id="id_parcela" name="id_parcela" <?php echo ($this->uri->segment(2) == 'cadastrar') ? '' : ''; ?>>
+                                        <select id="id_parcela" name="id_parcela" <?php echo ($this->uri->segment(2) == 'cadastrar') ? '' : 'disabled'; ?>>
                                             <option></option>                                         
                                             <?php
                                             if(!empty($infoParcelas))
