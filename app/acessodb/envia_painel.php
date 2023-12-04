ba841c4bde09ab95ad25e9ee536fbad57fdd8c2d
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-12-04 12:14:08 -0300

    ....

--- app/acessodb/envia_painel.php
+++ app/acessodb/envia_painel.php
@@ -143,10 +143,10 @@ if ($input['dscategoria'] == 'limpatabelas') {
 } else if ($input['dscategoria'] == 'epifitas') {
 		
 	$insertQuery  = "REPLACE INTO tb_epifitas(id,id_acesso,id_parcela,latitude_campo_gd,longitude_campo_gd,latitude_campo_gms,longitude_campo_gms,descricao,st_registro_ativo) VALUES (?,?,?,?,?,?,?,?,?)";
-	if($stmt = $con->prepare($insertQuery)){
+	if($stmt = $con->prepare($insertQuery)){		
+		$stmt->bind_param("sssssssss",$input['idcontroleepifitas'],$input['idacesso'],getBetween($input['idparcelaepifitas'],"(",")"),$input['latitudecampogd'],$input['longitudecampogd'],$result[0],$result[1],$input['descricao'],'S');
 		$file = 'epifitas.txt';
 		file_put_contents($file,json_encode($input), FILE_APPEND | LOCK_EX);
-		$stmt->bind_param("sssssssss",$input['idcontroleepifitas'],$input['idacesso'],getBetween($input['idparcelaepifitas'],"(",")"),$input['latitudecampogd'],$input['longitudecampogd'],$result[0],$result[1],$input['descricao'],'S');
 		$stmt->execute();
 		$response["status"] = 0;		
 		$response["message"] = "Enviado com sucesso!";
