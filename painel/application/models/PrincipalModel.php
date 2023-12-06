c540e4e790543d7684eaa9b49843b5fe37b64eaf
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-12-05 23:35:08 -0300

    ....

--- painel/application/models/PrincipalModel.php
+++ painel/application/models/PrincipalModel.php
@@ -1146,9 +1146,9 @@ function carregaInfoPermissao($IdPermissao)
 
         $this->db->reconnect();
         $this->db->start_cache();
-        $sql = "INSERT INTO \"tb_animais\" (\"id\", \"id_parcela\", \"id_acesso\", \"latitude_gms\",
-        \"longitude_gms\", \"id_tipo_observacao\", \"id_classificacao\", \"id_grau_protecao\", \"descricao\", \"latitude_gd\", \"longitude_gd\",
-        \"st_registro_ativo\") VALUES ({$infoAnimal['id']}, {$infoAnimal['id_parcela']}, '{$infoAnimal['id_acesso']}', '{$infoAnimal['latitude_gms']}', 
+        $sql = "INSERT INTO \`tb_animais\` (\`id\`, \`id_parcela\`, \`id_acesso\`, \`latitude_gms\`,
+        \`longitude_gms\`, \`id_tipo_observacao\`, \`id_classificacao\`, \`id_grau_protecao\`, \`descricao\`, \`latitude_gd\`, \`longitude_gd\`,
+        \`st_registro_ativo\`) VALUES ({$infoAnimal['id']}, {$infoAnimal['id_parcela']}, '{$infoAnimal['id_acesso']}', '{$infoAnimal['latitude_gms']}', 
         '{$infoAnimal['longitude_gms']}', {$infoAnimal['id_tipo_observacao']}, {$infoAnimal['id_classificacao']}, '{$infoAnimal['id_grau_protecao']}', 
         '{$infoAnimal['descricao']}', '{$infoAnimal['latitude_gd']}', '{$infoAnimal['longitude_gd']}', '{$infoAnimal['st_registro_ativo']}')";
         $query = $this->db->query($sql);
